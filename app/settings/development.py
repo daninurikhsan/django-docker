3a02231f6490212928f269dd809ce792f2b53f99
daninurikhsan <90901425+daninurikhsan@users.noreply.github.com>
2023-07-27 21:09:48 +0800

    Update:  settings

--- app/settings/development.py
+++ app/settings/development.py
@@ -3,19 +3,19 @@ from app.settings.base import *
 DEBUG = True
 # ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'safebox_docker',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': 3306,
-        'default-character-set': 'utf8',
-        'OPTIONS': {
-            'use_unicode' : True,
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'safebox_docker',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': 3306,
+#         'default-character-set': 'utf8',
+#         'OPTIONS': {
+#             'use_unicode' : True,
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+#         },
+#     }
+# }
 
--- app/settings/production.py
+++ app/settings/production.py
@@ -4,18 +4,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 DEBUG = False
 # ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE_NAME'),
-        'USER': os.environ.get('MYSQL_DATABASE_USER'),
-        'PASSWORD': os.environ.get('MYSQL_DATABASE_PASSWORD'),
-        'HOST': os.environ.get('MYSQL_DATABASE_HOST'),
-        'PORT': os.environ.get('MYSQL_DATABASE_PORT'),
-        'default-character-set': 'utf8',
-        'OPTIONS': {
-            'use_unicode' : True,
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('MYSQL_DATABASE_NAME'),
+#         'USER': os.environ.get('MYSQL_DATABASE_USER'),
+#         'PASSWORD': os.environ.get('MYSQL_DATABASE_PASSWORD'),
+#         'HOST': os.environ.get('MYSQL_DATABASE_HOST'),
+#         'PORT': os.environ.get('MYSQL_DATABASE_PORT'),
+#         'default-character-set': 'utf8',
+#         'OPTIONS': {
+#             'use_unicode' : True,
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+#         },
+#     }
+# }
