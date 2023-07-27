8f982a42e3edb8c71bd7ddb5d53ed1fa141ab2c1
daninurikhsan <90901425+daninurikhsan@users.noreply.github.com>
2023-07-28 01:35:08 +0800

    Update settings to default

--- app/settings.py
+++ app/settings.py
@@ -15,7 +15,9 @@ from dotenv import main
 import os
 
 main.load_dotenv()
+
 ALLOWED_HOSTS = ['*']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,6 +77,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE_NAME'),
+        'USER': os.environ.get('MYSQL_DATABASE_USER'),
+        'PASSWORD': os.environ.get('MYSQL_DATABASE_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_DATABASE_HOST'),
+        'PORT': os.environ.get('MYSQL_DATABASE_PORT'),
+        'default-character-set': 'utf8',
+        'OPTIONS': {
+            'use_unicode' : True,
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        },
+    }
+}
 
 
 # Password validation
